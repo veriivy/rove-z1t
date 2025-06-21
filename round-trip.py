68ae21ab848f72fce51384de144d0a5150233d27
MrGrian <117320420+MrGrian@users.noreply.github.com>
2025-06-21 11:36:11 -0400

    Add api key

--- round-trip.py
+++ round-trip.py
@@ -2,7 +2,10 @@ import http.client
 
 conn = http.client.HTTPSConnection("priceline-com-provider.p.rapidapi.com")
 
-headers = { 'x-rapidapi-host': "priceline-com-provider.p.rapidapi.com" }
+headers = {
+    'x-rapidapi-key': "ae154958cbmsh0156f277de1e385p171d11jsnbd9a365120e3",
+    'x-rapidapi-host': "priceline-com-provider.p.rapidapi.com"
+}
 
 conn.request("GET", "/v2/flight/roundTrip?sid=iSiX639&origin_airport_code=YWG%2CJFK&adults=1&departure_date=2021-12-21%2C2021-12-25&destination_airport_code=JFK%2CYWG", headers=headers)
 
