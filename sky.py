39467cc0f0c5605847b1735290dd89ff64042e72
AjThe007 <aaravjindal@gmail.com>
2025-06-22 15:22:13 -0400

    Update sky.py
    Fixed dates

--- sky.py
+++ sky.py
@@ -42,8 +42,8 @@ def get_data(dep_date, ret_date):
     response = requests.get(url, headers=headers, params=querystring)
     return response.json()
 
-start_date = datetime(2025, 5, 21).date()
-end_date = datetime(2025, 6, 21).date()
+start_date = datetime(2025, 6, 30).date()
+end_date = datetime(2025, 7, 30).date()
 trip_length = 4  # days between departure and return
 current_date = start_date
 
